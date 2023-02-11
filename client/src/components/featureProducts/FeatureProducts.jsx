b1df66df314be61be49bf12ab977fc7bd3aba8f7
sup25 <suparnaa51@gmail.com>
2023-02-11 22:31:57 +0545

    updated

--- client/src/components/featureProducts/FeatureProducts.jsx
+++ client/src/components/featureProducts/FeatureProducts.jsx
@@ -3,61 +3,17 @@ import './FeatureProduct.scss'
 import Card from "../card/Card";
 import axios from 'axios';
 const FeatureProducts = ({ type }) => {
-    const data = [
-        {
-            id: 0,
-            img: "https://source.unsplash.com/xw5cQNbky5A",
-            img2: 'https://source.unsplash.com/YsiSAp3ccvk',
-            title: "title",
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
 
-
-        },
-        {
-            id: 1,
-            img: "https://source.unsplash.com/2UTk-Nip5aM",
-            title: "title",
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-
-
-        },
-        {
-            id: 2,
-            img: "https://source.unsplash.com/3MvlGhagq4E",
-            title: "title",
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-
-
-        },
-        {
-            id: 3,
-            img: "https://source.unsplash.com/yQTCPf-AnUY",
-            title: "title",
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-
-
-        }
-
-
-
-    ];
-    const [products, setProducts] = useState([])
+    const [data, setData] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const data = await axios.get(process.env.REACT_APP_API_URL + "/products", {
-                    headers: { Authorization: "bearer" + process.env.REACT_APP_API_TOKEN, }
-                })
-                console.log(data)
+                const res = await axios.get(process.env.REACT_APP_API_URL + "/products?populate=*",
+                    {
+                        headers: { Authorization: "bearer " + process.env.REACT_APP_API_TOKEN, }
+                    })
+                setData(res.data.data)
 
             } catch (err) {
                 console.log(err)
@@ -66,6 +22,7 @@ const FeatureProducts = ({ type }) => {
         }
         fetchData();
     }, [])
+    console.log(data)
 
     return (
         <div className='featuredProducts'>
